72e3b3caddfdcbc57d0fd98a8a2165ca1afa4c37
Dell_PC_User <dmitriyhaydukov@gmail.com>
2021-09-04 12:02:12 +0700

    Holo shell.

--- HoloShell/HoloShell/Program.cs
+++ HoloShell/HoloShell/Program.cs
@@ -19,7 +19,6 @@ namespace HoloShell
         static void Main(string[] args)
         {
             Run();
-            //Test();
         }
 
         static void Run()
@@ -44,41 +43,6 @@ namespace HoloShell
             }
 
             Console.ReadLine();
-        }
-
-        static void Test()
-        {
-            TestCharting();
-        }
-
-        static void TestCharting()
-        {
-            using (MemoryMappedFile mmf = MemoryBaseProcessor.CreateMMF())
-            {
-                Chart chart = new Chart();
-                ChartSeries chartSeries = new ChartSeries()
-                {
-                    Points = new List<ChartPoint>()
-                    {
-                        new ChartPoint(1, 1),
-                        new ChartPoint(2, 6),
-                        new ChartPoint(3, 3),
-                        new ChartPoint(4, 7),
-                        new ChartPoint(5, 2),
-                        new ChartPoint(6, 5)
-                    }
-                };
-
-                chart.SeriesCollection = new List<ChartSeries>()
-                {
-                    chartSeries
-                };
-
-                ChartSerialization cs = new ChartSerialization();
-                MemoryWriter.Write<Chart>(chart, cs);
-
-                chart = MemoryReader.Read<Chart>(cs);
-            }
         }
     }
 }
