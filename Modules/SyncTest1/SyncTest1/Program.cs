a192004eb88d7b41d7d104ce6533893e0c4d158f
Dell_PC_User <dmitriyhaydukov@gmail.com>
2021-09-25 14:02:19 +0700

    Sync test applications.

---
+++ Modules/SyncTest1/SyncTest1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncTest1
+{
+    class Program
+    {
+        private const string SYNC_EVENT_NAME = "HoloSyncEvent";
+
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = Program.Process;
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+        }
+
+        private static void Process()
+        {
+            EventWaitHandle handle = new EventWaitHandle(false, EventResetMode.ManualReset, SYNC_EVENT_NAME);
+            while(handle.WaitOne())
+            {
+                Console.WriteLine("holo sync event");
+                handle.Reset();
+            }
+        }
+    }
+}
---
+++ Modules/SyncTest2/SyncTest2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SyncTest2
+{
+    class Program
+    {
+        private const string SYNC_EVENT_NAME = "HoloSyncEvent";
+
+        static void Main(string[] args)
+        {
+            EventWaitHandle handle = EventWaitHandle.OpenExisting(SYNC_EVENT_NAME);
+
+            string command = null;
+            while(!String.Equals(command, "q", StringComparison.OrdinalIgnoreCase))
+            {
+                command = Console.ReadLine();
+                if (command == "sync")
+                {
+                    handle.Set();
+                }
+            }
+        }
+    }
+}
