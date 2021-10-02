11af9486a64c3333ff519c5bb84b6ba37f7b4962
Dell_PC_User <dmitriyhaydukov@gmail.com>
2021-10-02 15:22:37 +0700

    Bitmap serialization.

---
+++ HoloCommon/HoloCommon/Serialization/Imaging/BitmapSerialization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using HoloCommon.MemoryManagement;
+
+using HoloCommon.Interfaces;
+
+namespace HoloCommon.Serialization.Imaging
+{
+    public class BitmapSerialization : IBinarySerialization<Bitmap>
+    {
+        public byte[] Serialize(Bitmap obj)
+        {
+            byte[] resBytes = null;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                obj.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                resBytes = stream.ToArray();
+            }
+
+            return resBytes; 
+        }
+
+        public Bitmap Deserialize(byte[] bytes)
+        {
+            Bitmap resBitmap = null;
+            using (MemoryStream stream = new MemoryStream(bytes))
+            {
+                Image image = Image.FromStream(stream);
+                resBitmap = new Bitmap(image);
+            }
+            return resBitmap;
+        }
+    }
+}
--- Modules/ImageViewer/ImageViewer/App.xaml.cs
+++ Modules/ImageViewer/ImageViewer/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using System.Threading;
 
 using HoloCommon.Serialization.Imaging;
 using HoloCommon.MemoryManagement;
@@ -19,14 +20,42 @@ namespace ImageViewer
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            IEnumerable<WriteableBitmap> images = MemoryReader.ReadCollection(new WriteableBitmapSerialization());
+            //IEnumerable<WriteableBitmap> images = MemoryReader.ReadCollection(new WriteableBitmapSerialization());
+            //MainViewModel mainViewModel = new MainViewModel();
+            //mainViewModel.MainImageSource = images.First();          
+            
             MainViewModel mainViewModel = new MainViewModel();
-            mainViewModel.MainImageSource = images.First();
-                        
 
+            Action action = () =>
+            {
+                mainViewModel.MainImageSource = ReadImage();
+            };
+
+            Thread thread = HoloCommon.Synchronization.SynchronizationManager.RunActionOnSignal(action, HoloCommon.Synchronization.Events.Camera.PICTURE_TAKEN);
+            
             MainWindow mainWindow = new MainWindow();
             mainWindow.ViewModel = mainViewModel;
+
+            mainWindow.Closed += (object ss, EventArgs args) =>
+            {
+                thread.Abort();
+            };
+
             mainWindow.Show();
+        }
+                
+        private WriteableBitmap ReadImage()
+        {
+            System.Drawing.Bitmap image = MemoryReader.Read<System.Drawing.Bitmap>(new BitmapSerialization());
+
+            System.Windows.Media.Imaging.BitmapSource bitmapSource =
+                System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                image.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty,
+                System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+            WriteableBitmap writeableBitmap = new WriteableBitmap(bitmapSource);
+
+            return writeableBitmap;
         }
     }
 }
